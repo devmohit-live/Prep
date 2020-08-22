53b9d0bf2e170e466d7b5380459a1aa4fefb8b48
Ankur Rai <rai.ankur0007@gmail.com>
2020-08-22 18:23:59 +0530

    Ankur | added unset kth value in Bit Manipulation

--- Bit Manipulation/kthBit.py
+++ Bit Manipulation/kthBit.py
@@ -1,4 +1,4 @@
-# getting or setting kth bit of any number
+print(unSetKthBit(3, 1))  # will make it 2# getting or setting kth bit of any number
 
 
 def getKthBit(n, k) -> bool:
@@ -11,10 +11,16 @@ def setKthBit(n, k) -> int:
     n = n | 1 << (k-1)
     return n
 
+def unSetKthBit(n, k) -> int:
+    n = n & ~(1 << (k - 1))
+    return n
+
 
 print(getKthBit(4, 1))  # =>0
 print(getKthBit(4, 3))  # =>1
 print(setKthBit(4, 1))  # 5 setting lastbit change 4 to 5
+print(unSetKthBit(3, 1))  # will make it 2    000011 ==> 000010
+print(unSetKthBit(4, 3))  # will make it 0    000100 ==> 000000
 
 
 # # python way @_@
