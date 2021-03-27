d126b2ea525ae7af028c6bef43d72ca6baf763e1
Mohit <mohitsingh.it5@gmail.com>
2021-03-27 14:58:27 +0530

    Modified code for edge cases -01Knapsack -DP | Mohit
    Modified code for cases:
    1. When creating 2 partiotns are not possible
    2. Arrays contains zero => can be added to any subset increasing count ans ways of forming subset
    3. S=> target sum given is greater than the actual sum of array

--- DP/01 Knapsack Type/OperationTargetSumCount.java
+++ DP/01 Knapsack Type/OperationTargetSumCount.java
@@ -3,6 +3,26 @@
  * operation you have to tell count many such ways are there such that on
  * changing sign we get the sum of array to the goven sum
  */
+// can't devide the subset into two parts
+// // [7,9,3,8,0,2,4,8,3,9]
+// 0
+// Output
+// 40
+// Expected
+// 0
+
+// Solution: check wheter subset can be made => (S+sum)%2 == 0 (even can be
+// divided only) else return 0 => same logicd is been applied to equal sum
+
+// Inp:
+// [1]
+// 2
+// solution: sum<S => not possible return 0
+
+// [1,0] => actual op = 2 => {1} {0} , {1,0} {}
+// code will give =>1
+// iterate j from 0 instead of 1, to count for 0's too
+
 public class OperationTargetSumCount {
     /**
      * It is exactly CountSubsetWithGivenDifference here there will be two subsets
@@ -13,8 +33,9 @@ public class OperationTargetSumCount {
      * @param args
      */
     public static void main(String[] args) {
-        int[] arr = { 1, 1, 2, 3 };
-        int target_sum = 1; // 3
+        // int[] arr = { 1, 1, 2, 3 };
+        int[] arr = { 1, 0 };
+        int target_sum = 1; // 2
         int sum = 0;
         for (int i : arr) {
             sum += i;
